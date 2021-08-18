1524b56f6aff343bc36e6ac0fec136e7c9b8f1f1
Mônica S <monica.s.bergues@gmail.com>
2021-08-18 15:46:59 -0300

    feat: delete group goals

--- src/Components/MyGoals/index.jsx
+++ src/Components/MyGoals/index.jsx
@@ -1,9 +1,14 @@
-import { Divider } from "antd";
+import { Button, Divider, Tooltip } from "antd";
 import { useMyGoals } from "../../Providers/myGoals/MyGoals";
 import { Container } from "./style";
+import { DeleteOutlined } from "@ant-design/icons";
 
 export const GoalsList = () => {
-  const { myGoals } = useMyGoals();
+  const { myGoals, DeleteGroupGoal } = useMyGoals();
+
+  const handleRemove = (id) => {
+    DeleteGroupGoal(id);
+  };
 
   return (
     <Container>
@@ -13,6 +18,14 @@ export const GoalsList = () => {
           <p>Meta: {elem.title}</p>
           <p>Dificuldade: {elem.difficulty}</p>
           <p>Alcançado: {elem.how_much_achieved}%</p>
+          <Tooltip title="Remover Metas">
+            <Button
+              shape="circle"
+              onClick={() => handleRemove(elem.id)}
+              icon={<DeleteOutlined />}
+              size="large"
+            />
+          </Tooltip>
         </>
       ))}
     </Container>
