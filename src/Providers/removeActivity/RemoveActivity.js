6bbfd27e4790f2e08c1ae4f38fe1920362d98ea0
joaopedro <jpss7693@gmai.com>
2021-08-19 17:05:11 -0300

    build: building feature to exclude activities

--- src/Providers/removeActivity/RemoveActivity.js
+++ src/Providers/removeActivity/RemoveActivity.js
@@ -1,26 +1,31 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 import api from "../../Services/api";
+import { useAuth } from "../auth/Auth";
+import { toast } from "react-toastify";
 
 const RemoveActivity = createContext();
 
-export const RemoveActivityProvider = ({children}) => {
+export const RemoveActivityProvider = ({ children }) => {
+  const { token } = useAuth();
 
+  const RemoveFromActivity = (id) => {
+    api
+      .delete(`/activities/${id}/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => {
+        toast.success("Atividade  Removida");
+      })
+      .catch((err) => console.log("deu erro"));
+  };
 
-    const RemoveFromActivity = (info) => {
-        api.delete(`/activities/${info.id}`,
-        {
-            headers: {
-                Authorization: `Bearer `,
-            }
-        }
-        )
-    }
-
-    return(
-        <RemoveActivity.Provider value={{ RemoveFromActivity}}>
-            {children}
-        </RemoveActivity.Provider>
-    );
-}
+  return (
+    <RemoveActivity.Provider value={{ RemoveFromActivity }}>
+      {children}
+    </RemoveActivity.Provider>
+  );
+};
 
 export const RemoveActivityContext = () => useContext(RemoveActivity);
